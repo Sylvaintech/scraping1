40eef454b27ee29676697c1304d875d86fe882b3
Sylvaintech <sylvainlunel.job@gmail.com>
2021-07-15 18:07:39 +0200

    fin exo 2

--- lib/mairie_chrystmas.rb
+++ lib/mairie_chrystmas.rb
@@ -1,3 +1,5 @@
+
+
 require 'nokogiri'
 require 'csv'
 require 'open-uri'
@@ -7,7 +9,7 @@ require 'open-uri'
 
 def nom_villes
 
-    doc = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html%22")) 
+    doc = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html")) 
     list_villes = []
 
     doc.css('td > p > a.lientxt').each do |ville|
@@ -20,7 +22,7 @@ end
     
 def lien_villes
 
-    doc = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html%22")) 
+    doc = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html")) 
     list_url = []
 
     doc.xpath('//a[contains(@href,"95")]/@href').each do |lien|
@@ -58,46 +60,40 @@ puts couple
 
 
 
+def list_val_oise
+
+    page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html"))
+    tab_commune = []
+
+    page.css('td > p > a.lientxt').each do |node|
+    puts node.text
+    end
+puts tab_commune
+end
 
 
+def couple
 
+list_villes = nom_villes
+list_url = lien_villes
+courriel = []
 
+list_url.each do |url_mairie|
+    doc = Nokogiri::HTML(URI.open(url_mairie))
+    doc.css("body > div > main > section:nth-child(2) > div > table > tbody > tr:nth-child(4) > td:nth-child(2)").each do |mail_mairie|
+        if mail_mairie.text == ""
+            str = "pas de mail"
+            courriel << str
+        else
+            courriel << mail_mairie.text
+        end
+    end
+end
 
+duo = list_villes.zip(courriel).map { |x, y| {x => y} }
 
-# def list_val_oise
-
-#     page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html"))
-#     tab_commune = []
-
-#     page.css('td > p > a.lientxt').each do |node|
-#     puts node.text
-#     end
-# puts tab_commune
-# end
-
-
-# def couple
-
-# list_villes = nom_villes
-# list_url = lien_villes
-# courriel = []
-
-# list_url.each do |url_mairie|
-#     doc = Nokogiri::HTML(URI.open(url_mairie))
-#     doc.css("body > div > main > section:nth-child(2) > div > table > tbody > tr:nth-child(4) > td:nth-child(2)").each do |mail_mairie|
-#         if mail_mairie.text == ""
-#             str = "pas de mail"
-#             courriel << str
-#         else
-#             courriel << mail_mairie.text
-#         end
-#     end
-# end
-
-# duo = list_villes.zip(courriel).map { |x, y| {x => y} }
-
-# return duo
-# end
+return duo
+end
 
 
 
